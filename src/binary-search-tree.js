d8ab667fa59091cc8b7e6113dbe9b9db5e5360c4
polikmm <polikmm@mail.ru>
2024-12-24 17:46:27 +0400

    feat: add constructor to binarySearchTree && root method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,19 +1,21 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.base = null;
   }
 
-  add(/* data */) {
+  root() {
+    return this.base;
+  }
+
+  add(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
